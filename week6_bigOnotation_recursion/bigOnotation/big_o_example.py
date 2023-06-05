14ae4cc59be6093ad5bd52649c4b32d6f7ae7297
Ubuntu <ubuntu@ip-172-31-15-233.us-west-1.compute.internal>
2023-06-05 18:51:38 +0000

    updated code - data5500 base code

--- week6_bigOnotation_recursion/bigOnotation/big_o_example.py
+++ week6_bigOnotation_recursion/bigOnotation/big_o_example.py
@@ -1,6 +1,8 @@
+import time
+
 lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9]
 
-
+# O(n^2) examples
 for l in lst:
     print(l, end=" ")
 
@@ -12,23 +14,26 @@ for l in lst:
     
 print("\nmean: ", tot / num_elem)
 
+# O(n^2) example timing it
+start = time.time()
+
+for i in range(1000):
+    for j in range(1000):
+        print(i * j)
+        
+end = time.time()
+print("runtime (seconds): ", end-start)
+
 # O(n^4)
 # for l in range(1000):
 #     for j in range(1000):
 #         for k in range(1000):
 #             for m in range(1000):
 #                 print(l * j * k * m, end=" ")
-                 
-# for i in range(1000):
-#     for j in range(1000):
-#         print(i * j)
-        
-# for m in range(1000):
-#     for n in range(1000):
-#         print(m * n)
+         
         
         
-        
+# another O(n^2) example
 lst = [[1,2,3], [3,4,5], [5,6,7], [8,9,0]]
 
 total = 0
@@ -38,3 +43,6 @@ for row in lst:
         total += item
         ct += 1
 print("average: ", total / ct)
+
+print("press enter to continue...")
+
